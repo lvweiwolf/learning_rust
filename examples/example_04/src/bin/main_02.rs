448bd53b0b2becfaf9b2eec57f78899f620d2414
lvwei <lvweiwolf@163.com>
2026-01-21 23:20:05 +0800

    添加变量绑定示例

---
+++ examples/example_04/src/bin/main_02.rs
@@ -0,0 +1,15 @@
+
+
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // 错误代码
+    // _immutable_binding += 1;     
+}
